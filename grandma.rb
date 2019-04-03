3edb6c9353f3846c14ee6402998529d511e80e4e
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-03 02:13:41 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,16 +1,4 @@
 
-def speak_to_grandma(phrase)
-    if phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
-      
-    elsif phrase.swapcase || phrase.downcase 
-  		return "HUH?! SPEAK UP, SONNY!"
-
-    else
-  		puts "NO, NOT SINCE 1938!"
-    end
-  
-end
 
 
 
