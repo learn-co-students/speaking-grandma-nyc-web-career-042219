82bf68f8bb65bd3cb926babb19dbfbffc2d905ac
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-03 19:17:30 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 
-def speak_to_grandma(string)
-  if string.upcase == "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase)
+  if phrase.upcase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   
 
