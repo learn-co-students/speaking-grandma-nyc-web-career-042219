4908fce0c93e633790d3f277bc01cf85bb78ae35
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-03 01:16:31 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,13 @@
+def speak_to_grandma
+  if 
+    puts
+  elseif
+    puts 
+  else
+    puts 
+  
+end
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
