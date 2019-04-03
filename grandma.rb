cd556eba30fff5b342158a7762f75b40dddb6577
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-03 19:25:01 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,11 @@
 
 def speak_to_grandma(phrase)
-  if phrase.upcase == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  
-
+  else if phrase == phrase.upcase
+    puts "NO, NOT SINCE 1938"
+    
+  else 
+    puts "HUH?! SPEAK UP, SONNY!"
+    end
 end
-
